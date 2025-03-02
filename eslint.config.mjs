fa201a252219be31ff7a3b3208c6b09a4aafdeb9
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-03-02 15:58:01 +0900

    refactor: 不要コメントアウト削除

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,18 +13,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-// /** @type {import('eslint').Linter.Config[]} */
-// export default [
-//   includeIgnoreFile(gitignorePath),
-//   {
-//     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-//   },
-//   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-//   pluginJs.configs.recommended,
-//   ...tseslint.configs.recommended,
-//   pluginReact.configs.flat.recommended,
-// ];
-
 export default tseslint.config(
   includeIgnoreFile(gitignorePath),
   pluginJs.configs.recommended,
@@ -85,23 +73,4 @@ export default tseslint.config(
       },
     },
   },
-  // {
-  //   name: "eslint-config-file",
-  //   files: ["eslint.config.mjs"],
-  //   languageOptions: {
-  //     globals: {
-  //       ...globals.browser,
-  //       ...globals.commonjs,
-  //       ...globals.node,
-  //       // TODO: あったほうが良いか検証する
-  //       // ...globals.es2015,
-  //     },
-  //   },
-  //   settings: {
-  //     "import/internal-regex": "^~/",
-  //     "import/resolver": {
-  //       typescript: {},
-  //     },
-  //   },
-  // },
 );
