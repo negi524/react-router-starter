8d8c322c3d50cae5ebf90fb8663b35b182c9d181
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-03-04 22:02:47 +0900

    feat: サインアウト機能実装

--- app/routes/signout/route.tsx
+++ app/routes/signout/route.tsx
@@ -1,10 +1,25 @@
+import { destroySession, getSession } from "~/sessions.server";
+import { Form, redirect } from "react-router";
+import type { Route } from "../signout/+types/route";
+
+export async function action({ request }: Route.ActionArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+  return redirect("/signin", {
+    headers: {
+      "Set-Cookie": await destroySession(session),
+    },
+  });
+}
+
 export default function SignOutRoute() {
   return (
     <div>
       <h1>Sign out</h1>
-      <button className="py-1 px-5 bg-sky-500 rounded-2xl text-white cursor-pointer">
-        サインアウト
-      </button>
+      <Form method="post">
+        <button className="py-1 px-5 bg-sky-500 rounded-2xl text-white cursor-pointer">
+          サインアウト
+        </button>
+      </Form>
     </div>
   );
 }
