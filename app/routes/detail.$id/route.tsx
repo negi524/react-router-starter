fd80276b4ee2b2ec24f7f6886d7a3d9180362ec3
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-03-05 23:52:48 +0900

    feat: 個別エラーハンドリング実装

--- app/routes/detail.$id/route.tsx
+++ app/routes/detail.$id/route.tsx
@@ -1,7 +1,19 @@
+import { data, isRouteErrorResponse } from "react-router";
 import type { Route } from "../detail.$id/+types/route";
 
 export async function loader({ params }: Route.LoaderArgs) {
   const num = Number(params.id);
+
+  if (!Number.isFinite(num)) {
+    throw new Error("数値ではありません");
+  }
+
+  if (num < 0 || 100 <= num) {
+    throw data("IDが見つかりません", {
+      status: 404,
+      statusText: "IDが見つかりません",
+    });
+  }
 
   return {
     id: num,
@@ -11,8 +23,32 @@ export async function loader({ params }: Route.LoaderArgs) {
 export default function DetailPageRoute({ loaderData }: Route.ComponentProps) {
   return (
     <div>
-      <h1 className="text-4xl">これは詳細ページ</h1>
+      <h2 className="text-3xl">これは詳細ページ</h2>
       <p>パラメータ:{loaderData.id}</p>
     </div>
   );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <>
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div>
+        <h1>エラー</h1>
+        <p>{error.message}</p>
+        <p>スタックトレースは次のとおりです:</p>
+        <pre>{error.stack}</pre>
+      </div>
+    );
+  } else {
+    return <h1>不明なエラー</h1>;
+  }
 }
