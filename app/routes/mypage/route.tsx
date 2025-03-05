8b2a9a25a34a2345bde31bc4fc714f8c6b3e53fd
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-03-05 18:28:44 +0900

    feat: ユーザーIDを表示するように修正

--- app/routes/mypage/route.tsx
+++ app/routes/mypage/route.tsx
@@ -1,7 +1,25 @@
-export default function MyPageRoute() {
+import { redirect } from "react-router";
+import { getSession } from "~/sessions.server";
+import type { Route } from "../mypage/+types/route";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+
+  if (!session.has("userId")) {
+    // サインインしていない場合は、サインインページにリダイレクト
+    return redirect("/signin");
+  }
+
+  return {
+    userId: session.get("userId"),
+  };
+}
+export default function MyPageRoute({ loaderData }: Route.ComponentProps) {
+  const { userId } = loaderData;
   return (
     <div>
-      <h1>This is MyPage.</h1>
+      <h1 className="text-4xl">This is MyPage.</h1>
+      <p>Your id is 「{userId}」</p>
     </div>
   );
 }
