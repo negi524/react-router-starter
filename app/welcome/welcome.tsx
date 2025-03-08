4484b2531d5b210902c81fb6a94d2aa1d71a304b
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-03-08 11:26:58 +0900

    feat: アイコン配置

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,9 +1,11 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import { CiMenuBurger } from "react-icons/ci";
 
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
+      <CiMenuBurger className="fixed top-0 right-0 p-3" size="4rem" />
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
