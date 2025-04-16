15efbc597b030f1aafdd57529464442668fd2deb
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-04-16 22:20:22 +0900

    feat: パスのエイリアスを修正

--- test/services/calcService.test.ts
+++ test/services/calcService.test.ts
@@ -1,6 +1,5 @@
 import { expect, test } from "vitest";
-// import { sum } from "~/services/calcService";
-import { sum } from "app/services/calcService";
+import { sum } from "~/services/calcService";
 
 test("adds 1 + 2 equals 3", () => {
   expect(sum(1, 2)).toBe(3);
