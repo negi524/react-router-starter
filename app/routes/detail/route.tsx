1b7fa192f4aaf07219b646dbdd72fb7235fd6ca9
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-04-30 13:03:51 +0900

    feat: 不要な記述削除

--- app/routes/detail/route.tsx
+++ app/routes/detail/route.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router";
-import type { Route } from "./+types/route";
 
 export async function loader() {
   return {
@@ -7,10 +6,7 @@ export async function loader() {
   };
 }
 
-export default function DetailListPageRoute({
-  loaderData,
-}: Route.ComponentProps) {
-  console.log({ loaderData });
+export default function DetailListPageRoute() {
   return (
     <div>
       <h1 className="text-4xl">これは一覧ページ</h1>
