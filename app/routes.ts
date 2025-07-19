d7bc0371ccf876f9cfc7cc3f26e46c3dd7fd5a62
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-07-19 11:34:29 +0900

    refactor: ルーティング方法変更

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,11 @@
-import { type RouteConfig } from "@react-router/dev/routes";
-import { flatRoutes } from "@react-router/fs-routes";
+import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
-export default flatRoutes() satisfies RouteConfig;
+export default [
+  index("./routes/index.tsx"),
+  route("sample", "./routes/sample.tsx"),
+  route("detail", "./routes/detailList.tsx"),
+  route("detail/:id", "./routes/detail.tsx"),
+  route("mypage", "./routes/mypage.tsx"),
+  route("signin", "./routes/signin.tsx"),
+  route("signout", "./routes/signout.tsx"),
+] satisfies RouteConfig;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from "../components/welcome/welcome";
 
 export function meta() {
   return [
