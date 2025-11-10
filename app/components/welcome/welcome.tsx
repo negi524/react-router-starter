ced7abc56e33c6e77c935ca6e7fafc665ac27e68
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-11-10 14:00:13 +0900

    feat: add submit button

--- app/components/welcome/welcome.tsx
+++ app/components/welcome/welcome.tsx
@@ -70,7 +70,6 @@ export function Welcome() {
                     <a
                       className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
                       href={href}
-                      target="_blank"
                       rel="noreferrer"
                     >
                       {text}
@@ -103,5 +102,9 @@ const resources = [
   {
     href: "/detail",
     text: "Detail",
+  },
+  {
+    href: "/edit",
+    text: "Edit",
   },
 ];
--- app/routes/edit.tsx
+++ app/routes/edit.tsx
@@ -1,4 +1,14 @@
 import { useEffect } from "react";
+import type { Route } from "./+types/edit";
+import { logger } from "~/config/logger";
+
+export async function action({ request }: Route.ActionArgs) {
+  // フォームの送信を処理するロジックをここに追加できます
+  const form = await request.formData();
+  const message = form.get("message");
+  logger.info(`Submitted message: ${message}`);
+  return null;
+}
 
 export default function EditPageRoute() {
   useEffect(() => {
@@ -12,9 +22,15 @@ export default function EditPageRoute() {
   return (
     <div>
       <h2>編集画面</h2>
-      <textarea id="message" name="message" className="border"></textarea>
-      <br />
-      <a href="/">Home</a>
+      <form method="POST">
+        <textarea id="message" name="message" className="border"></textarea>
+        <br />
+        <a href="/">Home</a>
+        <br />
+        <button className="cursor-pointer rounded-2xl bg-sky-500 px-5 py-1 text-white">
+          送信
+        </button>
+      </form>
     </div>
   );
 }
