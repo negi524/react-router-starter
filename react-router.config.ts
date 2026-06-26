9bef1ffd10c5adaa2a11e7e1e2314da8c0527605
negi524 <egtnrumpbiizarre.info@gmail.com>
2026-06-26 19:28:13 +0900

    feat: add feature flags

--- react-router.config.ts
+++ react-router.config.ts
@@ -6,5 +6,9 @@ export default {
   ssr: true,
   future: {
     v8_middleware: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+    v8_passThroughRequests: true,
+    v8_trailingSlashAwareDataRequests: true,
   },
 } satisfies Config;
